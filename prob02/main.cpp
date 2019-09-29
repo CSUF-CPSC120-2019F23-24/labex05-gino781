0b8b1142ceb7e6f0e1de860c38125918bbe3c212
Gino Angiuli <gino.angiuli@csu.fullerton.edu>
2019-09-29 14:24:31 -0700

    Username

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-  std:;string first_name;
+  std::string first_name;
     std::cout << "Enter your first name: ";
   getline(std::cin, first_name);
 
@@ -14,13 +14,13 @@ int main()
   std::cout << "Enter the first initial of your middle name: ";
   std::cin.get(middle_initial);
 
-std::cout << "Enter the first initial of your last name: ";
-  last_initial = std::cin.get();
+  std::cout << "Enter the first initial of your last name: ";
   std::cin.ignore();
+  std::cin.get(last_initial);
 
   // create username out of user's first name and middle/last initials
-  std::string 2username = first_name+middle_initial+last_initial;
-  std::cout >> "Your user name is \"" << username << ""\n";
+  std::string username = first_name + middle_initial + last_initial;
+  std::cout << "Your user name is \"" << username << "\n";
 
   return 0;
 }
